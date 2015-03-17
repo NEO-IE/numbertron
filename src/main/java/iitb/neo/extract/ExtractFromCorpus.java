fab8d25d363a70eb840b2ea089a8ded048ad77b9
Aman Madaan <madaan.amanmadaan@gmail.com>
2015-03-18 00:36:56 +0530

    fix bug, add confidence

--- src/main/java/iitb/neo/extract/ExtractFromCorpus.java
+++ src/main/java/iitb/neo/extract/ExtractFromCorpus.java
@@ -1,4 +1,5 @@
 package main.java.iitb.neo.extract;
+
 import iitb.rbased.meta.RelationMetadata;
 import iitb.shared.EntryWithScore;
 
@@ -7,9 +8,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -26,7 +28,6 @@ import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.util.CoreMap;
 import edu.stanford.nlp.util.Pair;
-import edu.washington.multir.util.EvaluationUtils;
 import edu.washington.multir.util.ModelUtils;
 import edu.washington.multirframework.argumentidentification.ArgumentIdentification;
 import edu.washington.multirframework.argumentidentification.SententialInstanceGeneration;
@@ -40,7 +41,6 @@ import edu.washington.multirframework.data.Argument;
 import edu.washington.multirframework.data.Extraction;
 import edu.washington.multirframework.featuregeneration.FeatureGenerator;
 import eval.UnitExtractor;
-
 
 /**
  * The main method of this class will print to an output file all the
@@ -59,57 +59,51 @@ public class ExtractFromCorpus {
 	private String corpusPath;
 	private UnitExtractor ue;
 	private Set<String> relations;
-	
-	
 
 	public ExtractFromCorpus(String propertiesFile) throws Exception {
-		String jsonProperties = IOUtils.toString(new FileInputStream(new File(
-				propertiesFile)));
+		String jsonProperties = IOUtils.toString(new FileInputStream(new File(propertiesFile)));
 		Map<String, Object> properties = JsonReader.jsonToMaps(jsonProperties);
-		corpusPath = getStringProperty(properties,"corpusPath");
-		
+		corpusPath = getStringProperty(properties, "corpusPath");
+
 		String featureGeneratorClass = getStringProperty(properties, "fg");
 		if (featureGeneratorClass != null) {
-			fg = (FeatureGenerator) ClassLoader.getSystemClassLoader()
-					.loadClass(featureGeneratorClass).newInstance();
+			fg = (FeatureGenerator) ClassLoader.getSystemClassLoader().loadClass(featureGeneratorClass).newInstance();
 		}
 
 		String aiClass = getStringProperty(properties, "ai");
 		if (aiClass != null) {
-			ai = (ArgumentIdentification) ClassLoader.getSystemClassLoader()
-					.loadClass(aiClass).getMethod("getInstance").invoke(null);
+			ai = (ArgumentIdentification) ClassLoader.getSystemClassLoader().loadClass(aiClass)
+					.getMethod("getInstance").invoke(null);
 		}
 		List<String> sigClasses = getListProperty(properties, "sigs");
 		sigs = new ArrayList<>();
 		for (String sigClass : sigClasses) {
-			sigs.add((SententialInstanceGeneration) ClassLoader
-					.getSystemClassLoader().loadClass(sigClass)
+			sigs.add((SententialInstanceGeneration) ClassLoader.getSystemClassLoader().loadClass(sigClass)
 					.getMethod("getInstance").invoke(null));
 		}
 		multirDirs = new ArrayList<>();
-		List<String> multirDirNames = getListProperty(properties,"models");
-		for(String multirDirName : multirDirNames){
+		List<String> multirDirNames = getListProperty(properties, "models");
+		for (String multirDirName : multirDirNames) {
 			multirDirs.add(multirDirName);
 		}
 		cis = new CustomCorpusInformationSpecification();
-		
-		String altCisString = getStringProperty(properties,"cis");
-		if(altCisString != null){
-			cis = (CustomCorpusInformationSpecification)ClassLoader.getSystemClassLoader().loadClass(altCisString).newInstance();
+
+		String altCisString = getStringProperty(properties, "cis");
+		if (altCisString != null) {
+			cis = (CustomCorpusInformationSpecification) ClassLoader.getSystemClassLoader().loadClass(altCisString)
+					.newInstance();
 		}
 		ue = new UnitExtractor();
 		relations = RelationMetadata.getRelations();
 	}
 
-	
 	public static void main(String[] args) throws Exception {
-	
-		
+
 		ExtractFromCorpus efc = new ExtractFromCorpus(args[0]);
 		Corpus c = new Corpus(efc.corpusPath, efc.cis, true);
 		c.setCorpusToDefault();
 		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("rulebased_extractions")));
-		
+
 		List<Extraction> extrs = efc.getExtractions(c, efc.ai, efc.fg, efc.sigs, efc.multirDirs, bw);
 		System.out.println("Total extractions : " + extrs.size());
 		bw.close();
@@ -127,8 +121,7 @@ public class ExtractFromCorpus {
 		return null;
 	}
 
-	public static String formatExtractionString(Corpus c, Extraction e)
-			throws SQLException {
+	public static String formatExtractionString(Corpus c, Extraction e) throws SQLException {
 		StringBuilder sb = new StringBuilder();
 		String[] eValues = e.toString().split("\t");
 		String arg1Name = eValues[0];
@@ -172,158 +165,180 @@ public class ExtractFromCorpus {
 		return sb.toString().trim();
 
 	}
-	
-	
 
-	public List<Extraction> getExtractions(Corpus c,
-			ArgumentIdentification ai, FeatureGenerator fg,
-			List<SententialInstanceGeneration> sigs, List<String> modelPaths,
-			BufferedWriter bw) throws SQLException, IOException {
+	public List<Extraction> getExtractions(Corpus c, ArgumentIdentification ai, FeatureGenerator fg,
+			List<SententialInstanceGeneration> sigs, List<String> modelPaths, BufferedWriter bw) throws SQLException,
+			IOException {
 		BufferedWriter featureWriter = new BufferedWriter(new FileWriter("extract_features"));
-		
+
 		List<Extraction> extrs = new ArrayList<Extraction>();
 		for (int i = 0; i < sigs.size(); i++) {
 			Iterator<Annotation> docs = c.getDocumentIterator();
 			SententialInstanceGeneration sig = sigs.get(i);
 			String modelPath = modelPaths.get(i);
 			SentLevelExtractor sle = new SentLevelExtractor(modelPath, fg, ai, sig);
-			
+
 			Map<String, Integer> rel2RelIdMap = sle.getMapping().getRel2RelID();
-			
-			Map<Integer, String> ftID2ftMap = ModelUtils
-					.getFeatureIDToFeatureMap(sle.getMapping());
-		
-//			PrintWriter pw1 = new PrintWriter(new FileWriter("features_map"));
-//			for(Integer id : ftID2ftMap.keySet()) {
-//				pw1.write(id.toString() + " - " + ftID2ftMap.get(id) + "\n");
-//			}
-//			pw1.close();
+
+			Map<Integer, String> ftID2ftMap = ModelUtils.getFeatureIDToFeatureMap(sle.getMapping());
+
+			// PrintWriter pw1 = new PrintWriter(new
+			// FileWriter("features_map"));
+			// for(Integer id : ftID2ftMap.keySet()) {
+			// pw1.write(id.toString() + " - " + ftID2ftMap.get(id) + "\n");
+			// }
+			// pw1.close();
 			int docCount = 0;
 			while (docs.hasNext()) {
 				Annotation doc = docs.next();
-				List<CoreMap> sentences = doc
-						.get(CoreAnnotations.SentencesAnnotation.class);
+				List<CoreMap> sentences = doc.get(CoreAnnotations.SentencesAnnotation.class);
 				for (CoreMap sentence : sentences) {
 					// argument identification
-					List<Argument> arguments = ai.identifyArguments(doc,
-							sentence);
+					List<Argument> arguments = ai.identifyArguments(doc, sentence);
 					// sentential instance generation
-					List<Pair<Argument, Argument>> sententialInstances = sig
-							.generateSententialInstances(arguments, sentence);
+					List<Pair<Argument, Argument>> sententialInstances = sig.generateSententialInstances(arguments,
+							sentence);
 					for (Pair<Argument, Argument> p : sententialInstances) {
-						 if(!(exactlyOneNumber(p) && secondNumber(p))) { // do not waste time with useless extractions
-							 continue;
+						if (!(exactlyOneNumber(p) && secondNumber(p))) { // do
+																			// not
+																			// waste
+																			// time
+																			// with
+																			// useless
+																			// extractions
+							continue;
 						}
 						Map<Integer, Double> perRelationScoreMap = sle
-								.extractFromSententialInstanceWithAllRelationScores(
-										p.first, p.second, sentence, doc, featureWriter);
-						ArrayList<Integer> compatRels = unitsCompatible(p.second, sentence, sle.getMapping().getRel2RelID());
+								.extractFromSententialInstanceWithAllRelationScores(p.first, p.second, sentence, doc,
+										featureWriter);
+						ArrayList<Integer> compatRels = unitsCompatible(p.second, sentence, sle.getMapping()
+								.getRel2RelID());
 						String relStr = null;
 						Double extrScore = -1.0;
-						for(Integer rel: perRelationScoreMap.keySet()) { //sorted by score
-							if(compatRels.contains(rel)) {
+						for (Integer rel : perRelationScoreMap.keySet()) { // sorted
+																			// by
+																			// score
+							if (compatRels.contains(rel)) {
 								relStr = sle.relID2rel.get(rel);
-								if(!sle.relID2rel.keySet().contains(rel)) {
+								if (!sle.relID2rel.keySet().contains(rel)) {
 									System.err.println("Outlier: " + rel);
 								}
 								extrScore = perRelationScoreMap.get(rel);
 							}
 						}
-						if(relStr == null) { //no compatible extraction 
+						String senText = sentence.get(CoreAnnotations.TextAnnotation.class);
+						String docName = sentence.get(SentDocName.class);
+
+						Integer sentNum = sentence.get(SentGlobalID.class);
+						if (extrScore <= 0) { // no compatible extraction ||
+												// only negative extractions
+							// System.out.println(relStr + " : " + extrScore +
+							// "\t" + senText);
 							continue;
 						}
-								//System.out.println(extrResult);
-																//prepare extraction
-								String docName = sentence
-										.get(SentDocName.class);
-									String senText = sentence
-										.get(CoreAnnotations.TextAnnotation.class);
-								Integer sentNum = sentence
-										.get(SentGlobalID.class);
-								Extraction e = new Extraction(p.first,
-										p.second, docName, relStr, sentNum,
-										extrScore, senText);
-								extrs.add(e);
-								System.out.println(relStr + " : " + extrScore);
-								bw.write(formatExtractionString(c, e) + "\n");
+						// System.out.println(extrResult);
+						// prepare extraction
+
+						double max, min;
+						ArrayList<Double> scores =  new ArrayList<Double>(perRelationScoreMap.values());
+						max = min = scores.get(0);
+						for(int i1 = 1, l = scores.size(); i1 < l; i1++) {
+							if(max < scores.get(i1)) {
+								max = scores.get(i1);
+							}
+							if(min > scores.get(i1)) {
+								min = scores.get(i1);
+							}
+							
 						}
 
-						}
+						double conf = (extrScore - min) / (max - min);
+						Extraction e = new Extraction(p.first, p.second, docName, relStr, sentNum, extrScore, senText);
+
+						extrs.add(e);
+
+						bw.write(formatExtractionString(c, e) + " "  + conf + "\n" );
 					}
-				
-			
-				docCount++;
-				if (docCount % 100 == 0) {
-					System.out.println(docCount + " docs processed");
-					bw.flush();
-				}	
+
+				}
 			}
-			
-		
+
+			docCount++;
+			if (docCount % 100 == 0) {
+				System.out.println(docCount + " docs processed");
+				bw.flush();
+			}
+		}
 
 		return extrs;
 	}
-	
+
 	private static boolean exactlyOneNumber(Pair<Argument, Argument> p) {
 		boolean firstHasNumber = p.first.getArgName().matches(".*\\d.*");
 		boolean secondHasNumber = p.second.getArgName().matches(".*\\d.*");
 		return firstHasNumber ^ secondHasNumber;
 	}
-	
+
 	private static boolean secondNumber(Pair<Argument, Argument> p) {
 		boolean secondHasNumber = p.second.getArgName().matches(".*\\d.*");
 		return secondHasNumber;
 	}
-	
+
 	/*
 	 * returns the list of relation compatible with numeric argument
 	 */
-	private ArrayList<Integer> unitsCompatible(Argument numArg, CoreMap sentence, Map<String, Integer> map){
+	private ArrayList<Integer> unitsCompatible(Argument numArg, CoreMap sentence, Map<String, Integer> map) {
 		String sentString = sentence.toString();
 		String tokenStr = numArg.getArgName();
 		String parts[] = tokenStr.split("\\s+");
 		int beginIdx = sentString.indexOf(parts[0]);
 		int endIdx = beginIdx + parts[0].length();
-		
+
 		String front = sentString.substring(0, beginIdx);
-		if(front.length() > 20){
-			front = front.substring(front.length()-20);
+		if (front.length() > 20) {
+			front = front.substring(front.length() - 20);
 		}
 		String back = sentString.substring(endIdx);
-		if(back.length() > 20){
+		if (back.length() > 20) {
 			back = back.substring(0, 20);
 		}
-		String utString = front + "<b>" + parts[0] + "</b>" + back; 
+		String utString = front + "<b>" + parts[0] + "</b>" + back;
 		float values[][] = new float[1][1];
 		List<? extends EntryWithScore<Unit>> unitsS = ue.parser.getTopKUnitsValues(utString, "b", 1, 0, values);
-		
+
 		// check for unit here....
-		
+
 		String unit = "";
 		if (unitsS != null && unitsS.size() > 0) {
 			unit = unitsS.get(0).getKey().getBaseName();
 		}
-		
+
 		ArrayList<Integer> validRelations = new ArrayList<Integer>();
-		for(String rel: relations){
-			if(unitRelationMatch(rel, unit)){
+		for (String rel : relations) {
+			if (unitRelationMatch(rel, unit)) {
 				validRelations.add(map.get(rel));
 			}
 		}
 		return validRelations;
 	}
-	
-	public boolean unitRelationMatch(String rel, String unitStr){
+
+	public boolean unitRelationMatch(String rel, String unitStr) {
 		Unit unit = ue.quantDict.getUnitFromBaseName(unitStr);
 		if (unit != null && !unit.getBaseName().equals("")) {
 			Unit SIUnit = unit.getParentQuantity().getCanonicalUnit();
 			if (SIUnit != null && !RelationMetadata.getUnit(rel).equals(SIUnit.getBaseName()) || SIUnit == null
 					&& !RelationMetadata.getUnit(rel).equals(unit.getBaseName())) {
 				return false; // Incorrect unit, this cannot be the relation.
-								
+
 			}
-		} else if (unit == null && !unitStr.equals("")
-				&& RelationMetadata.getUnit(rel).equals(unitStr)) { // for the cases where units are compound units.
+		} else if (unit == null && !unitStr.equals("") && RelationMetadata.getUnit(rel).equals(unitStr)) { // for
+																											// the
+																											// cases
+																											// where
+																											// units
+																											// are
+																											// compound
+																											// units.
 			return true;
 		} else {
 			if (!RelationMetadata.getUnit(rel).equals("")) {
@@ -332,16 +347,16 @@ public class ExtractFromCorpus {
 		}
 		return true;
 	}
-	
+
 	private static double sigmoid(double score) {
 		return 1 / (1 + Math.exp(-score));
 	}
-	private List<String> getListProperty(Map<String, Object> properties,
-			String string) {
-		if(properties.containsKey(string)){
+
+	private List<String> getListProperty(Map<String, Object> properties, String string) {
+		if (properties.containsKey(string)) {
 			JsonObject obj = (JsonObject) properties.get(string);
 			List<String> returnValues = new ArrayList<>();
-			for(Object o : obj.getArray()){
+			for (Object o : obj.getArray()) {
 				returnValues.add(o.toString());
 			}
 			return returnValues;
