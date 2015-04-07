bf04f440a48602baa5c38fdd826d67ace55acee7
Aman Madaan <madaan.amanmadaan@gmail.com>
2015-04-07 20:06:23 +0530

    fixed typo

--- src/main/java/iitb/neo/pretrain/featuregeneration/NumberFeatureGenerator.java
+++ src/main/java/iitb/neo/pretrain/featuregeneration/NumberFeatureGenerator.java
@@ -10,64 +10,59 @@ import edu.washington.multirframework.featuregeneration.FeatureGenerator;
 
 /**
  * A class that generates the number features
+ * 
  * @author ashish
- *
+ * 
  */
 public class NumberFeatureGenerator implements FeatureGenerator {
 
-	public List<String> generateFeatures(Integer first, Integer second,
-			Integer first2, Integer second2, String arg1id, String arg2id,
-			CoreMap sentence, Annotation doc) {
+	public List<String> generateFeatures(Integer first, Integer second, Integer first2, Integer second2, String arg1id,
+			String arg2id, CoreMap sentence, Annotation doc) {
 
-		
-		//List<CoreLabel> tokens = sentence.get(CoreAnnotations.TokensAnnotation.class);
+		// List<CoreLabel> tokens =
+		// sentence.get(CoreAnnotations.TokensAnnotation.class);
 		List<String> features = new ArrayList<String>();
 		Double value = Number.getDoubleValue(arg2id);
-		if(value == null){
-			return features; //no valid number could be formed of this string.
+		if (value == null) {
+			return features; // no valid number could be formed of this string.
 		}
-		
-		if(value < 0){
+
+		if (value < 0) {
 			features.add("Num: Neg");
 		}
-		
-		if (value > 0 && value < 1){
+
+		if (value > 0 && value < 1) {
 			features.add("Num: Bet0And1");
-		}
-		else if(value == 1){
+		} else if (value == 1) {
 			features.add("Num: One");
-		}
-		else if(value > 1 && value < 10){
+		} else if (value > 1 && value < 10) {
 			features.add("Num: Units");
-		}
-		else if(value >= 10 && value < 100){
+		} else if (value >= 10 && value < 100) {
 			features.add("Num: Tens");
-		}
-		else if(value >= 100 && value < 1000){
+		} else if (value >= 100 && value < 1000) {
 			features.add("Num: Hundreds");
-		}else if (value >= 1000 && value < 10000){
+		} else if (value >= 1000 && value < 10000) {
 			features.add("Num: Thousands");
-		}else if(value >= 10000 && value < 100000){
+		} else if (value >= 10000 && value < 100000) {
 			features.add("Num: Ten Thousands");
-		}else if( value >= 100000 && value < 1000000){
+		} else if (value >= 100000 && value < 1000000) {
 			features.add("Num: Lac");
-		}else if(value >= 1000000 && value < 1000000000){
+		} else if (value >= 1000000 && value < 1000000000) {
 			features.add("Num: Million");
-		}else if(value >= 1000000000 && value < 1000000000000.0){
+		} else if (value >= 1000000000 && value < 1000000000000.0) {
 			features.add("Num: Billion");
-		}else if (value >= 1000000000000.0){
+		} else if (value >= 1000000000000.0) {
 			features.add("Num: Tera");
 		}
-		
+
 		if ((value == Math.floor(value)) && !Double.isInfinite(value)) {
-		    features.add("Num: Integer");
+			features.add("Num: Integer");
 		}
-		
+
 		/*
 		 * todo: add scientific notation feature
 		 */
-		
-		
+
 		return features;
 	}
 
