a56f2f1512bb83ce0470ba0f2d4eccb20d627195
Aman Madaan <madaan.amanmadaan@gmail.com>
2015-05-18 12:30:31 +0530

    Add active inference

--- src/main/java/iitb/neo/training/algorithm/lpercp/ActiveInference.java
+++ src/main/java/iitb/neo/training/algorithm/lpercp/ActiveInference.java
@@ -1,6 +1,10 @@
 package main.java.iitb.neo.training.algorithm.lpercp;
 
+import java.util.HashSet;
+import java.util.List;
+
 import main.java.iitb.neo.training.ds.LRGraph;
+import main.java.iitb.neo.training.ds.Number;
 
 public class ActiveInference {
 
@@ -10,10 +14,26 @@ public class ActiveInference {
 		p.z_states = new boolean[lrg.Z.length];
 		p.n_states = new boolean[lrg.n.length];
 		int numN = lrg.n.length;
+		Integer labelTrue = LocalAveragedPerceptron.featNameNumMapping.get("hard: "+ lrg.relation);
+		Integer labelFalse = LocalAveragedPerceptron.featNameNumMapping.get("hard: N_"+ lrg.relation);
 		for (int n_i = 0; n_i < numN; n_i++) {
 			if (GoldDbInference.closeEnough(lrg.n[n_i].value, lrg.relation, lrg.location)) {
+				HashSet<Integer> feats = new HashSet<>();
+				Number n = lrg.n[n_i];
+				List<Integer> z_s = n.zs_linked;
+				for(Integer z: z_s) {
+					for(int id: lrg.features[z].ids){
+						feats.add(id);
+					}
+				}
+				if(feats.contains(labelTrue)) {
+					p.n_states[n_i] = true;
+				} else if (feats.contains(labelFalse)){
+					p.n_states[n_i] = false;
+				} else {
+					p.n_states[n_i] = true;
+				}
 				
-				p.n_states[n_i] = true;
 			} else {
 				p.n_states[n_i] = false;
 			}
